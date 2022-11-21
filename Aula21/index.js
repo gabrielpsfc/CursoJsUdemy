3831ba959f3c7ca4ee9daef4c78d9985f3b5a6f5
Gabriel Pereira Soares <gabriel.soares.forcode@gmail.com>
2022-11-20 21:35:45 -0300

    Miniatura da aula 15:48 / 15:48 37. Operadores de comparação

---
+++ Aula21/index.js
@@ -0,0 +1,20 @@
+/**
+ * Operadores de comparação
+ * (> maior que, >= maior que ou igual a, < menor que, <= menor que ou igual a, == igualdade (valor), === igualdade estrita (valor e tipo), != diferente (valor), !== diferente estrito (valor e tipo))
+ * 
+ */
+const comp = 10 >  5
+console.log(10 > 5)
+console.log(comp)
+const compa = 10 >= 10
+console.log(compa)
+console.log(10 >= 11)
+console.log(10 < 11)
+console.log(12 <= 11)
+var num1 = 10
+var num2 = 11
+console.log(num1 <= num2)
+let numero = 10
+let numero1 = '10'
+console.log(numero == numero1)
+console.log(numero === numero1)
