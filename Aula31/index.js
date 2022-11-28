1214a4a8c207d3fef6300290db9f3010458aa805
Gabriel Pereira Soares <gabriel.soares.forcode@gmail.com>
2022-11-28 20:37:48 -0300

    49. Mais diferenças entre var e let/const

---
+++ Aula31/index.js
@@ -0,0 +1,42 @@
+//let nome = 'Otávio' let não pode ser redeclarada
+//var nome2 = 'Otávio' var pode ser redeclarada
+// let tem escopo de bloco { ... bloco}
+// var tem escopo de função
+
+/*let nome = 'Luiz' // criando
+var nome2 = 'Luiz' //criando
+
+var nome2 = 'Otávio'
+
+const verdadeira = true
+
+if (verdadeira) {
+    let nome = 'Otávio' //criando
+    var nome2 = 'Rogerio'
+  
+
+    if (verdadeira) {
+        let nome ='Outra coisa'
+        var nome2 = 'Ronaldo'
+        console.log(nome, nome2)
+        
+        //console.log(nome, nome2)
+    }
+}
+
+console.log(nome, nome2)*/
+
+//var sobrenome = 'Miranda'
+
+//function falaOi (){
+//    var nome = 'Luiz'
+//    console.log(sobrenome)
+//}
+//falaOi()
+
+// var retorna indefined quando declarada depois de acionada, let retorna erro
+
+
+console.log(sobrenome)
+
+let sobrenome ='Miranda'
