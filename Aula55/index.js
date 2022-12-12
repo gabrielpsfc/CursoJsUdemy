a336e1e900c43e6dc7c8786e0d41688031343fb0
Gabriel Pereira Soares <gabriel.soares.forcode@gmail.com>
2022-12-11 22:33:04 -0300

    74. Funções imediatas (IIFE)

---
+++ Aula55/index.js
@@ -0,0 +1,26 @@
+//funções auto invocadas IIFE -  Immediately invoked function expression
+(function(){
+
+})()
+
+
+/*(function(){
+    const nome = 'Luiz'
+    console.log(nome)
+    
+})();*/
+
+(function(idade, peso, altura){
+    const sobrenome = 'Miranda'
+    function criaNome(nome){
+        return nome + ' ' + sobrenome
+    }
+
+    function falaNome(){
+        console.log(criaNome('Luiz'))
+    }
+    falaNome()
+    console.log(idade, peso, altura)
+})(30,80,1.80)
+
+
