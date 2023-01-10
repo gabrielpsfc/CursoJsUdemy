1d4f67f150bd0ed04231f7b3ca64527a1e4cfa7c
Gabriel Pereira Soares <gabriel.soares.forcode@gmail.com>
2023-01-09 23:35:00 -0300

    112. Fetch API (GET)

---
+++ Aula90 112. Fetch API (GET)/assets/js/main.js
@@ -0,0 +1,41 @@
+document.addEventListener('click', e => {
+    const el = e.target
+    const tag = el.tagName.toLowerCase()
+    if (tag === 'a') {
+        e.preventDefault()
+        carregaPagina(el)
+    }
+})
+
+async function carregaPagina(el) {
+    try {
+        const href = el.getAttribute('href')
+        const response = await fetch(href)
+        if (response.status !== 200) throw new Error('ERROR 404')
+        const html = await response.text()
+        carregaResultado(html)
+    } catch (e) {
+        console.log(e)
+    }
+
+}
+
+function carregaResultado(response) {
+    const resultado = document.querySelector('.resultado')
+    resultado.innerHTML = response
+}
+
+fetch('Pagina1.html')
+    .then(resposta => {
+        if (resposta.status !== 200) throw new Error('ERROR 404 NOSSO')
+        return resposta.text()
+    })
+    .then(html => console.log(html))
+    .catch(e => console.error(e))
+
+
+
+
+
+
+
